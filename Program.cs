dcfab26cf4141c054ca44958e07a52f2a8bbb66b
jimburrell <85350719+jimburrell@users.noreply.github.com>
2021-12-11 19:47:15 +0000

    yes

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SampleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guid myuuid = Guid.NewGuid();
+            string myuuidAsString = myuuid.ToString();
+
+            Console.WriteLine("Your UUID is: " + myuuidAsString);
+            Console.WriteLine("=====================================================");
+            Console.WriteLine("Press ENTER to EXIT");
+            Console.Read();  
+        }
+    }
+}
